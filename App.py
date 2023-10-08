89c537c09b558c06f6ae526c4ec421bc221bb2dd
Bryan <breilly.caster@gmail.com>
2023-10-08 13:22:18 -0700

    refactored birthday name

--- App.py
+++ App.py
@@ -1,52 +1,146 @@
 from flask import Flask, render_template, request
+import json
 
 app = Flask(__name__)
 
-all_rooms = {'Blue': {'name': '', 'greeting': ''}, 'Red': {'name': '', 'greeting': ''}, 'Yellow': {'name': '', 'greeting': ''}, 'Orange': {'name': '', 'greeting': ''}}
+all_rooms = {
+    "Blue": {"name": "", "greeting": "", "background": "fireworks.mp4"},
+    "Red": {"name": "", "greeting": "", "background": "fireworks.mp4"},
+    "Yellow": {"name": "", "greeting": "", "background": "fireworks.mp4"},
+    "Orange": {"name": "", "greeting": "", "background": "fireworks.mp4"},
+}
 
-@app.route("/", methods = ['GET', 'POST'])
+
+@app.route("/", methods=["GET", "POST"])
 def create_room():
-    if request.method == 'POST':
-        if request.form.get('Create Page') == 'Create Page':
-           greeting = request.form.get('greeting')
-           color = request.form.get('color')
-           name = request.form.get('name')
-           all_rooms[color]['greeting'] = greeting
-           all_rooms[color]['name'] = name
-    return render_template('create_name.html')
+    if request.method == "POST":
+        if request.form.get("Create Page") == "Create Page":
+            greeting = request.form.get("greeting")
+            color = request.form.get("color")
+            name = request.form.get("name")
+            background = request.form.get("background")
+            print(background)
+            all_rooms[color]["greeting"] = greeting
+            all_rooms[color]["name"] = name
+            all_rooms[color]["background"] = background
+    return render_template("create_name.html")
 
-@app.route("/BlueRoom", methods = ['GET', 'POST'])
+
+@app.route("/BlueRoom", methods=["GET", "POST"])
 def br():
-    getName = all_rooms['Blue']['name']
-    if getName == 'Name' or getName == '':
-        return render_template('index.html')
-    getGreeting = all_rooms['Blue']['greeting']
-    return render_template('blue.html', name=getName, greeting=getGreeting)
+    color = "blue"
+    css = f"{color}.css"
+    getName = all_rooms["Blue"]["name"]
+    getGreeting = all_rooms["Blue"]["greeting"]
+    background = all_rooms["Blue"]["background"]
+    if background != 'fireworks.mp4':
+        css = f"{color}diff.css"
+    return render_template(
+        "index.html",
+        name=getName,
+        greeting=getGreeting,
+        color=color,
+        css=css,
+        background=background,
+    )
 
-@app.route("/RedRoom", methods = ['GET', 'POST'])
+
+@app.route("/Getblue", methods=["GET"])
+def get_blue():
+    data = {
+        "greeting": all_rooms["Blue"]["greeting"],
+        "name": all_rooms["Blue"]["name"],
+        "background": all_rooms["Blue"]["background"],
+    }
+    return json.dumps(data)
+
+
+@app.route("/RedRoom", methods=["GET", "POST"])
 def rr():
-    getName = all_rooms['Red']['name']
-    if getName == 'Name' or getName == '':
-        return render_template('index.html')
-    getGreeting = all_rooms['Red']['greeting']
-    return render_template('red.html', name=getName, greeting=getGreeting)
+    color = "red"
+    css = f"{color}.css"
+    getName = all_rooms["Red"]["name"]
+    getGreeting = all_rooms["Red"]["greeting"]
+    background = all_rooms["Red"]["background"]
+    if background != 'fireworks.mp4':
+        css = f"{color}diff.css"
+    return render_template(
+        "index.html",
+        name=getName,
+        greeting=getGreeting,
+        color=color,
+        css=css,
+        background=background,
+    )
 
-@app.route("/YellowRoom", methods = ['GET', 'POST'])
+
+@app.route("/Getred", methods=["GET"])
+def get_red():
+    data = {
+        "greeting": all_rooms["Red"]["greeting"],
+        "name": all_rooms["Red"]["name"],
+        "background": all_rooms["Red"]["background"],
+    }
+    return json.dumps(data)
+
+
+@app.route("/YellowRoom", methods=["GET", "POST"])
 def yr():
-    getName = all_rooms['Yellow']['name']
-    if getName == 'Name' or getName == '':
-        return render_template('index.html')
-    getGreeting = all_rooms['Yellow']['greeting']
-    return render_template('yellow.html', name=getName, greeting=getGreeting)
+    color = "yellow"
+    css = f"{color}.css"
+    getName = all_rooms["Yellow"]["name"]
+    getGreeting = all_rooms["Yellow"]["greeting"]
+    background = all_rooms["Yellow"]["background"]
+    if background != 'fireworks.mp4':
+        css = f"{color}diff.css"
+    return render_template(
+        "index.html",
+        name=getName,
+        greeting=getGreeting,
+        color=color,
+        css=css,
+        background=background,
+    )
 
-@app.route("/OrangeRoom", methods = ['GET', 'POST'])
+
+@app.route("/Getyellow", methods=["GET"])
+def get_yellow():
+    data = {
+        "greeting": all_rooms["Yellow"]["greeting"],
+        "name": all_rooms["Yellow"]["name"],
+        "background": all_rooms["Yellow"]["background"],
+    }
+    return json.dumps(data)
+
+
+@app.route("/OrangeRoom", methods=["GET", "POST"])
 def orange_r():
-    getName = all_rooms['Orange']['name']
-    if getName == 'Name' or getName == '':
-        return render_template('index.html')
-    getGreeting = all_rooms['Orange']['greeting']
-    return render_template('orange.html', name=getName, greeting=getGreeting)
+    color = "orange"
+    css = f"{color}.css"
+    getName = all_rooms["Orange"]["name"]
+    getGreeting = all_rooms["Orange"]["greeting"]
+    background = all_rooms["Orange"]["background"]
+    if background != 'fireworks.mp4':
+        css = f"{color}diff.css"
+    return render_template(
+        "index.html",
+        name=getName,
+        greeting=getGreeting,
+        color=color,
+        css=css,
+        background=background,
+    )
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+@app.route("/Getorange", methods=["GET"])
+def get_orange():
+    data = {
+        "greeting": all_rooms["Orange"]["greeting"],
+        "name": all_rooms["Orange"]["name"],
+        "background": all_rooms["Orange"]["background"],
+    }
+    return json.dumps(data)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 Birthday Party Names as a python local server instead of an exe app
 
 Requires Flask
+- pip install Flask
+
+Run server with 
+python -m flask run --host=0.0.0.0
